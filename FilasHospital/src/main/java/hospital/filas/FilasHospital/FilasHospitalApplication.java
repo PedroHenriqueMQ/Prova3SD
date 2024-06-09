828634aa0f758aa56af9252ce2960841d26ec7b8
PedroHenriqueMQ <pedro_q777@live.com>
2024-06-08 23:50:01 -0300

    Fase inicial: Configurando application.propierties Filas_Hospital

---
+++ FilasHospital/src/main/java/hospital/filas/FilasHospital/FilasHospitalApplication.java
@@ -0,0 +1,13 @@
+package hospital.filas.FilasHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilasHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilasHospitalApplication.class, args);
+	}
+
+}
---
+++ FilasHospital/src/test/java/hospital/filas/FilasHospital/FilasHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.filas.FilasHospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilasHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
